915fad0c5f339143acdfcd4d49db9a63b5a36248
YunusovKirill <yunusov.ki@yandex.ru>
2024-09-04 22:46:09 +0400

    Добавлено событие на кнопку Меню - переход на главную страницу

--- footer.js
+++ footer.js
@@ -2,13 +2,17 @@ const footer = document.createElement('footer');
 
 footer.innerHTML = `
     <nav class="footer">
-        <button class="footer__btn">Меню</button>
+        <button id="footer__btn-menu" class="footer__btn">Меню</button>
         <button class="footer__btn">Сканер</button>
         <button id="footer__btn-back" class="footer__btn">Назад</button>
     </nav>
 `;
 
 document.body.appendChild(footer);
+
+document.getElementById('footer__btn-menu').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
 
 document.getElementById('footer__btn-back').addEventListener('click', function() {
     history.back();
